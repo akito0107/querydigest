05c7018be72e034e3af812b3a569219d2ded1230
Akito Ito <akito_ito@r.recruit.co.jp>
2019-08-02 13:52:17 +0900

    update main

--- main.go
+++ main.go
@@ -5,23 +5,22 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
-
-	"github.com/akito0107/xsqlparser/sqlast"
 )
 
 type queryTime struct {
-	QueryTime    float32
-	LockTime     float32
+	QueryTime    float64
+	LockTime     float64
 	RowsSent     int
 	RowsExamined int
 }
 
 type SlowQueryInfo struct {
-	Query     *sqlast.SQLStmt
+	RawQuery  []string
 	Time      time.Time
-	QueryTime queryTime
+	QueryTime *queryTime
 }
 
 func main() {
@@ -33,7 +32,6 @@ func main() {
 
 	reader := bufio.NewReaderSize(f, 4096)
 
-	var isQueryBlock bool
 	var slowqueries []SlowQueryInfo
 	for {
 		line, _, err := reader.ReadLine()
@@ -43,8 +41,62 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if strings.HasPrefix(string(line), "#") {
-			isQueryBlock = true
+		if !strings.HasPrefix(string(line), "# Time:") {
+			continue
 		}
+
+		strs := strings.Split(string(line), " ")
+		var slowquery SlowQueryInfo
+
+		t, err := time.Parse("2006-01-02T15:04:05.000000Z", strs[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		slowquery.Time = t
+		nextLine(reader)
+
+		slowquery.QueryTime = parseQueryTime(nextLine(reader))
+
+		slowqueries = append(slowqueries, slowquery)
+	}
+}
+
+func nextLine(reader *bufio.Reader) string {
+	l, _, err := reader.ReadLine()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(l)
+}
+
+func parseQueryTime(str string) *queryTime {
+
+	queryTimes := strings.Split(str, " ")
+	// Query_time
+	qt, err := strconv.ParseFloat(queryTimes[2], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Lock_time
+	lt, err := strconv.ParseFloat(queryTimes[5], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Rows_sent
+	rs, err := strconv.ParseInt(queryTimes[7], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Rows_examined
+	re, err := strconv.ParseInt(queryTimes[10], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &queryTime{
+		QueryTime:    qt,
+		LockTime:     lt,
+		RowsSent:     int(rs),
+		RowsExamined: int(re),
 	}
 }
