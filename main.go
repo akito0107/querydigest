a9812d1026484069339c75ebf2ce9fdfbc3cb3c1
Akito Ito <akito_ito@r.recruit.co.jp>
2019-08-02 12:41:59 +0900

    update

--- main.go
+++ main.go
@@ -5,7 +5,24 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/akito0107/xsqlparser/sqlast"
 )
+
+type queryTime struct {
+	QueryTime    float32
+	LockTime     float32
+	RowsSent     int
+	RowsExamined int
+}
+
+type SlowQueryInfo struct {
+	Query     *sqlast.SQLStmt
+	Time      time.Time
+	QueryTime queryTime
+}
 
 func main() {
 	f, err := os.Open("./slow.log")
@@ -17,12 +34,17 @@ func main() {
 	reader := bufio.NewReaderSize(f, 4096)
 
 	var isQueryBlock bool
+	var slowqueries []SlowQueryInfo
 	for {
 		line, _, err := reader.ReadLine()
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			log.Fatal(err)
+		}
+
+		if strings.HasPrefix(string(line), "#") {
+			isQueryBlock = true
 		}
 	}
 }
