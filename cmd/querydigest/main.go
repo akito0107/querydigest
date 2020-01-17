ed8f407df26b0dcd83278aa66217294db1d06a4c
Akito Ito <akito0107@gmail.com>
2020-01-17 19:47:22 +0900

    main

--- cmd/querydigest/main.go
+++ cmd/querydigest/main.go
@@ -9,6 +9,8 @@ import (
 	"runtime"
 	"sync"
 
+	"github.com/pkg/profile"
+
 	"github.com/akito0107/querydigest"
 )
 
@@ -17,8 +19,7 @@ var previewSize = flag.Int("n", 0, "count")
 var concurrency = flag.Int("j", 0, "concurrency (default = num of cpus)")
 
 func main() {
-	// defer profile.Start(profile.ProfilePath("."), profile.TraceProfile).Stop()
-	// defer profile.Start(profile.ProfilePath("."), profile.CPUProfile, profile.MemProfile).Stop()
+	defer profile.Start(profile.ProfilePath("."), profile.TraceProfile).Stop()
 	// defer profile.Start(profile.ProfilePath("."), profile.CPUProfile).Stop()
 	flag.Parse()
 
